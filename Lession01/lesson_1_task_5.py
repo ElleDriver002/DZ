474488e2c38f41bff7b7a991850c467d4ab67ef3
ElleDriver002 <Olya-se@mail.ru>
2026-09-10 18:33:06 +0300

    исправила02

--- Lession01/lesson_1_task_5.py
+++ Lession01/lesson_1_task_5.py
@@ -1,4 +1,14 @@
-def print_phone(phone):
-    print(phone)
+def print_num(num):
+    print(num, end='')
 
-print_phone(88005553535)
+print_num(8)
+print_num(8)
+print_num(0)
+print_num(0)
+print_num(5)
+print_num(5)
+print_num(3)
+print_num(5)
+print_num(3)
+print_num(5)
+print_num(5)
